e4f2f7e5f43519cbe534d8fab1b350862e5e0758
Oronde W <oronde.williams@pinnsg.com>
2023-01-23 15:16:50 +0000

    added delivery team seed and added it to stg_orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,5 +1,6 @@
 SELECT 
  --from raw_orders
+ {{ dbt_utils.generate_surrogate_key(['O.ORDERID', 'C.CUSTOMERID', 'P.PRODUCTID']) }} AS SK_ORDERS,
  O.ORDERID ,
  O.ORDERDATE ,
  O.SHIPDATE ,
@@ -17,9 +18,13 @@ SELECT
  P.CATEGORY, 
  P.PRODUCTNAME,
  P.SUBCATEGORY,
- {{ markup('ORDERSELLINGPRICE','ORDERCOSTPRICE') }} AS MARKUP
+ {{ markup('ORDERSELLINGPRICE','ORDERCOSTPRICE') }} AS MARKUP,
+ D.delivery_team
 FROM {{ ref('raw_orders') }} AS O
  LEFT JOIN {{ ref('raw_customer') }} AS C
   ON O.CUSTOMERID = C.CUSTOMERID
  LEFT JOIN {{ ref('raw_product') }} AS P
   ON O.PRODUCTID = P.PRODUCTID
+LEFT JOIN  {{ ref('delivery_team') }} AS D 
+  ON O.SHIPMODE = D.SHIPMODE
+--{{ limit_data_in_dev('ORDERDATE') }}
